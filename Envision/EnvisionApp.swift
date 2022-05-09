70868da86936c8918091a62a94eccdf5ab74eb4f
Michael Alfano <michaelalfano2004@gmail.com>
2022-05-09 19:15:30 -0400

    Add files via upload

---
+++ Envision/EnvisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvisionApp.swift
+//  Envision
+//
+//  Created by Michael Arthur Alfano on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct EnvisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
