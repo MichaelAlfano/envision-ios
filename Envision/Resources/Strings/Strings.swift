b84207854e586193fee9232ff17e88f2abd256bd
MichaelAlfano <michaelalfano2004@gmail.com>
2022-05-09 22:10:02 -0400

    added strings

---
+++ Envision/Resources/Strings/Strings.swift
@@ -0,0 +1,41 @@
+//
+//  Strings.swift
+//  Envision
+//
+//  Created by Michael Arthur Alfano on 5/9/22.
+//
+
+import Foundation
+
+class Strings {
+    struct Home {
+        static var SCREEN_TITLE = "Welcome"
+        static var SCREEN_SUBTITLE = "The following items require your action."
+    }
+    
+    struct Interests {
+        static var TIME = "2 mins"
+        static var TITLE = "Choose Interests"
+    }
+    
+    struct Profile {
+        static var TIME = "10 mins"
+        static var TITLE = "Setup Profile"
+    }
+    
+    struct Professionals {
+        static var TITLE = "Industry Professionals"
+        static var SUBTITLE_ONE = "Filter by"
+        static var SUBTITLE_TWO = "Interests"
+        static var SUBTITLE_THREE = "or"
+        static var SUBTITLE_FOUR = "All"
+    }
+    
+    struct MadeBy {
+        static var TITLE = "Envision was made by"
+        static var NAME = "Michael Alfano"
+    }
+    
+    static var VIEW_ALL = "View All"
+    static var RESOURCES_TITLE = "Resources"
+}
