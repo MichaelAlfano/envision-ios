64e7904c6c15e0eebe4ad387a13441e87df019f2
MichaelAlfano <michaelalfano2004@gmail.com>
2022-05-22 20:09:26 -0400

    Added additional resources

--- Envision/Data/Content/Resources.swift
+++ Envision/Data/Content/Resources.swift
@@ -36,6 +36,41 @@ struct Content {
             subtitle: "Matthew Blackman",
             paragraph: "Universe and More has a collection of interactive Physics resources such as Polarity Shift, Energy Bar Charts and more.",
             url: URL(string: "https://www.universeandmore.com/")
+        ),
+        ResourceCellData(
+            imageName: AppAssets.Images.aapt,
+            title: "AAPT",
+            subtitle: "American Association of Physics Teachers",
+            paragraph: "“A professional membership association of scientists dedicated to enhancing the understanding and appreciation of physics through teaching.“",
+            url: URL(string: "https://www.aapt.org/")
+        ),
+        ResourceCellData(
+            imageName: AppAssets.Images.donLincoln,
+            title: "Dr. Don Lincoln",
+            subtitle: "Educational Videos",
+            paragraph: "YouTube videos and Ted Talks by physicist Dr. Don Lincoln on particle physics, cosmology, and more.",
+            url: URL(string: "https://drdonlincoln.com/videos")
+        ),
+        ResourceCellData(
+            imageName: AppAssets.Images.sixtySymbols,
+            title: "Sixty Symbols",
+            subtitle: "Educational Videos",
+            paragraph: "“Videos describing the various symbols used in physics and astronomy.“",
+            url: URL(string: "https://www.youtube.com/user/sixtysymbols/featured")
+        ),
+        ResourceCellData(
+            imageName: AppAssets.Images.veritasium,
+            title: "Veritasium",
+            subtitle: "Educational Videos",
+            paragraph: "“A channel of science and engineering videos featuring experiments, expert interviews, cool demos, and discussions with the public about everything science.“",
+            url: URL(string: "https://www.veritasium.com/videos")
+        ),
+        ResourceCellData(
+            imageName: AppAssets.Images.pbs,
+            title: "PBS Space Time",
+            subtitle: "Educational Videos",
+            paragraph: "“Space Time explores the outer reaches of space, the craziness of astrophysics, the possibilities of sci-fi, and anything else you can think of beyond Planet Earth.“",
+            url: URL(string: "https://www.pbs.org/show/pbs-space-time/")
         )
     ]
 }
